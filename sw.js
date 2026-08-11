58c53cea4529bc69765691c4e7c6b0feba564bf0
tolipoff-git <tolipoff@github.com>
2026-08-11 09:12:10 -0400

    fix(print): preserve original container and force solid black color in print iframe

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'inv-inventory-';
-const CACHE_VERSION = 'v43';
+const CACHE_VERSION = 'v44';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const ASSETS_TO_CACHE = [
   './',
