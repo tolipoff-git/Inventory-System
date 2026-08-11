b0f207a1108123af9104ba85e58a43624f8ded88
tolipoff-git <tolipoff@github.com>
2026-08-11 11:08:24 -0400

    fix(pwa): handle edge case where service worker is already in waiting state on page load

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'inv-inventory-';
-const CACHE_VERSION = 'v45';
+const CACHE_VERSION = 'v46';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const ASSETS_TO_CACHE = [
   './',
