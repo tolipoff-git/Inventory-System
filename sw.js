a5d822748419ea22fb1e1d6a53cecf968e1cf88e
tolipoff-git <tolipoff@github.com>
2026-08-13 14:31:26 -0400

    Fix mobile layout, update loc parser, bump version to v65

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'inv-inventory-';
-const CACHE_VERSION = 'v64';
+const CACHE_VERSION = 'v65';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const ASSETS_TO_CACHE = [
   './',
