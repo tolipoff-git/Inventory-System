3438bc7a80ba38dbeff51fde87e240702c896b01
tolipoff-git <tolipoff@github.com>
2026-08-13 14:21:28 -0400

    fix: remove prefixes from label location display

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'inv-inventory-';
-const CACHE_VERSION = 'v63';
+const CACHE_VERSION = 'v64';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const ASSETS_TO_CACHE = [
   './',
