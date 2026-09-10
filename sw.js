5398aa4a81cb8c609aeca0c536678d7e7bfd2bac
tolipoff-git <tolipoff@github.com>
2026-09-10 10:36:48 -0400

    chore(pwa): stamp sw.js with latest commit hash

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'inv-inventory-';
-const CACHE_VERSION = 'v97-ebbd630';
+const CACHE_VERSION = 'v97-ee3664e';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const ASSETS_TO_CACHE = [
   './',
