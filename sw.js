85c173d3bf9e6e6f0422afbe4892f621a65c02c0
tolipoff-git <tolipoff@github.com>
2026-09-21 08:54:41 -0400

    chore(pwa): refresh sw.js cache version for v124

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'inv-inventory-';
-const CACHE_VERSION = 'v123-482e61c';
+const CACHE_VERSION = 'v124-e3fc59e';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const ASSETS_TO_CACHE = [
   './',
